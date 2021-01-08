08c06392e61fcf740693cd808e253e02850394a6
victorpyatakov <victor.pyatakov95@yandex.ru>
2021-01-08 18:16:03 +0300

    initial commit

---
+++ exampleIherit.py
@@ -0,0 +1,25 @@
+class Father:
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+
+    def print_name(self):
+        print(self.name, self.lastname)
+
+
+class Son(Father):
+    def __init__(self, name, lastname):
+        Father.__init__(self, name, lastname)
+
+
+class Daughter(Father):
+    def __init__(self, name, lastname):
+        # super() - позволяет обращаться к методам родителя без указания его имени
+        super().__init__(name, lastname)
+
+
+if __name__ == "__main__":
+    father = Father("Jon", "Week")
+    son = Son("Jack", "Week")
+    daughter = Daughter("Linda", "Week")
+    father.print_name()
